74bafdb386086ba932bda8cfad785d813634923f
Frost199 <emmaldini12@gmail.com>
2020-09-23 14:02:08 +0100

    feat: Add list ingredients feature

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -3,7 +3,7 @@ Serializers for the recipe resources
 """
 from rest_framework import serializers
 
-from core.models import Tag
+from core.models import Tag, Ingredient
 
 
 class TagSerializer(serializers.ModelSerializer):
@@ -17,4 +17,18 @@ class TagSerializer(serializers.ModelSerializer):
         """
         model = Tag
         fields = ('id', 'name')
+        read_only_fields = ('id',)
+
+
+class IngredientSerializer(serializers.ModelSerializer):
+    """
+    Serializer for ingredients object
+    """
+
+    class Meta:
+        """
+        Meta object for ingredients serializer
+        """
+        model = Ingredient
+        fields = ('id', 'name',)
         read_only_fields = ('id',)
