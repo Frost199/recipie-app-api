cc14835dd3cfdb046fbdfd7684bf1dcfed5c26f5
Frost199 <emmaldini12@gmail.com>
2020-09-24 09:02:53 +0100

    refactor: Modified the migrations

---
+++ app/core/migrations/Helper/helper.py
@@ -0,0 +1,20 @@
+"""
+Base Migration Module
+"""
+from django.conf import settings
+from django.db import migrations, models
+import django.db.models.deletion
+
+
+def create_fields():
+    """
+    create fields in migration
+    """
+    return [
+        ('id',
+         models.AutoField(auto_created=True, primary_key=True, serialize=False,
+                          verbose_name='ID')),
+        ('name', models.CharField(max_length=255)),
+        ('user', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE,
+                                   to=settings.AUTH_USER_MODEL)),
+    ]
